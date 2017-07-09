e54b1954bd1143b423a3a1c32323796be9587f0b
shannonphu <shannonphu@sbcglobal.net>
2017-07-08 21:41:30 -0700

    CourseJSONWriter maintains requisites when writing if the file already exists

--- CourseJSONWriter/CourseJSONWriter.py
+++ CourseJSONWriter/CourseJSONWriter.py
@@ -1,6 +1,14 @@
-import json
+import json, os
 
 class CourseJSONWriter(object):
 	def writeJSONToFile(self, courses_json, output_path):
+		# Retain manually input prerequisite/corequisite fields if they have been input before
+		if os.path.exists(output_path):
+			with open(output_path) as json_data:
+			    courses = json.load(json_data)
+			    for course_name in courses:
+					courses_json[course_name]['prerequisites'] = courses[course_name]['prerequisites']
+					courses_json[course_name]['corequisites'] = courses[course_name]['corequisites']
+
 		with open(output_path, 'w') as outfile:
 		    json.dump(courses_json, outfile, indent=4, sort_keys=True)
